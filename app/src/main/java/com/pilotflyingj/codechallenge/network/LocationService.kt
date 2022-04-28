7a6eca1f3fccba7bceed2fa36009bf24f24fabca
JMNolan <johnstaylownolan@gmail.com>
2022-04-28 14:09:05 -0500

    Initial commit

---
+++ app/src/main/java/com/pilotflyingj/codechallenge/network/LocationService.kt
@@ -0,0 +1,6 @@
+package com.pilotflyingj.codechallenge.network
+
+interface LocationService {
+    // TODO: define the API endpoint here, use the models in network layer with Kotlin Serialization
+    // Use https://drive.google.com/file/d/14IG3sYCHRAU5WyKcfgTUw9LS1Oo0DBF4/view?usp=sharing
+}
